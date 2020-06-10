c3de3071e22f9365f32854b0d90490483ef6fa2a
Francesca Ngatuvai <Chekkabekka@Talis-MacBook-Pro.local>
2020-06-10 14:02:25 -0600

    added card-component

--- src/index.js
+++ src/index.js
@@ -2,28 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 12:00PM"
-        content="I loved your blog!"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Hevani"
-        timeAgo="Today at 11:00AM"
-        content="When will you post again?"
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Cheka"
-        timeAgo="Yesterday at 6:00PM"
-        content="What a load of crap!"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 12:00PM"
+          content="I loved your blog!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Hevani"
+          timeAgo="Today at 11:00AM"
+          content="When will you post again?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Cheka"
+          timeAgo="Yesterday at 6:00PM"
+          content="What a load of crap!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
