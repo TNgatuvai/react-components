3a0eb9d55db61ee690dac8b56800cac4bcff9be7
Francesca Ngatuvai <Chekkabekka@Talis-MacBook-Pro.local>
2020-06-10 11:44:49 -0600

    Updates

---
+++ precache-manifest.68fa2288bdef9f4868f37c47081f6196.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ede9e33771be51d234f9abf657d9e81",
+    "url": "/react-components/index.html"
+  },
+  {
+    "revision": "bb4fccdea6e1d79d4035",
+    "url": "/react-components/static/js/2.0b57ec41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-components/static/js/2.0b57ec41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2627f6baeacbfa544d5",
+    "url": "/react-components/static/js/main.bb988338.chunk.js"
+  },
+  {
+    "revision": "0a9fb1aac421f591db98",
+    "url": "/react-components/static/js/runtime-main.5d3636cd.js"
+  }
+]);
